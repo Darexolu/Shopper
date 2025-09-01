c778aad55ba58e117b4b54c3110afc4ad197964a
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2025-09-01 18:30:53 +0100

    addToCart and frontend design complete remaining website responsiveness

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import {ShopContext} from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
       <div className="cartitems-format-main">
@@ -16,12 +16,11 @@ const CartItems = () => {
         <p>Remove</p>
       </div>
       <hr />
-      
       {all_product.map((e) => {
     if (cartItems[e.id] > 0) {
         return (
           <div key={e.id}>
-            <div className="cartitems-format">
+            <div className="cartitems-format cartitems-format-main">
               <img src={e.image} alt="" className='carticon-product-icon' />
               <p>{e.name}</p>
               <p>${e.new_price}</p>
@@ -30,6 +29,7 @@ const CartItems = () => {
               </button>
               <p>${e.new_price * cartItems[e.id]}</p>
               <img
+              className='cartitems-remove-icon'
                 src={remove_icon}
                 alt="remove"
                 onClick={() => removeFromCart(e.id)}
@@ -41,9 +41,38 @@ const CartItems = () => {
     }
     return null; 
 })}
-
+     <div className="cartitems-down">
+      <div className="cartitems-total">
+        <h1>Cart Totals</h1>
+        <div>
+          <div className="cartitems-total-item">
+            <p>Subtotal</p>
+            <p>${getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cartitems-total-item">
+            <p>Shipping Fee</p>  
+            <p>Free</p>
+          </div>
+          <hr />
+          <div className="cartitems-total-item">
+          <h3>Total</h3>
+          <h3>${getTotalCartAmount()}</h3>
+          </div>
+        </div>
+        <button>PROCEED TO CHECKOUT</button>
+      </div>
+      <div className="cartitems-promocode">
+        <p>If you have a promo code, Enter it here</p>
+        <div className="cartitems-promobox">
+          <input type="text" placeholder='promo code' />
+          <button>Submit</button>
+        </div>
+      </div>
+     </div>
     </div>
   )
 }
 
 export default CartItems
+// 2hrs 39mins 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React, { useState} from 'react'
+import React, { useContext, useState} from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
 
 const Navbar = () => {
 
 const [menu,setmenu] = useState('shop');
+const {getTotalCartItems} = useContext(ShopContext);
 
   return (
     <div>
@@ -25,7 +27,7 @@ const [menu,setmenu] = useState('shop');
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">
-            0
+            {getTotalCartItems()}
         </div>
        </div>
       </div>
@@ -34,3 +36,5 @@ const [menu,setmenu] = useState('shop');
 }
 
 export default Navbar
+
+//2hrs 58mins(front end design complete remaining the responsiveness)
