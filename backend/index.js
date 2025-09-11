2536d54cee79d3c3d231a6c7c702df3dcedd5233
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2025-09-11 13:22:00 +0100

    Initial commit backend

---
+++ backend/index.js
@@ -0,0 +1,104 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { type } = require("os");
+
+app.use(express.json());
+app.use(cors());
+
+//Database Connection With MongoDB
+mongoose.connect("mongodb+srv://darexolu_db:darexolu16@cluster0.p6p6s38.mongodb.net/ecommerce")
+
+// API Creation
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on Port "+port)
+    }
+    else{
+        console.log("Error : "+error)
+    }
+})
+
+//Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb)=>{
+       return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+//Creating upload Endpoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_ur: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for Creating Products
+
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required: true,
+    },
+     name:{
+        type: String,
+        required: true,
+    },
+     image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    }
+})
+
+app.post('/addproduct',async (req,res)=>{
+      const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+      });
+      console.log(product);
+      await product.save();
+      console.log("Saved");
+      res.json({
+        success: true,
+        name: req.body.name,
+      })
+})
